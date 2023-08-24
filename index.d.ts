788abb8d0d0d5673dd860640152eb71ec2992d09
Casper <casper@ext.store>
2023-08-24 09:09:01 +0200

    simplified interfaces

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@
 declare namespace ext.tabs {
 
   /** Tab object. */
-  export interface ExtTab {
+  export interface Tab {
     /** Tab ID. */
     id: string
     /** Owning extension ID. */
@@ -22,7 +22,7 @@ declare namespace ext.tabs {
   }
 
   /** Tab creation properties. */
-  export interface ExtTabProperties {
+  export interface TabProperties {
     /** Zero based tab index. */
     index?: number
     /** Icon URL or local path. */
@@ -38,7 +38,7 @@ declare namespace ext.tabs {
   }
 
   /** Tab event. */
-  export interface ExtTabEvent {
+  export interface TabEvent {
     /** Tab ID. */
     id: string
     /** Owning extension ID. */
@@ -46,7 +46,7 @@ declare namespace ext.tabs {
   }
 
   /** Event handler. */
-  export interface ExtTabHandler<Listener> {
+  interface EventHandler<Listener> {
     /**
      * Register listener.
      * @param listener Listener to invoke.
@@ -65,21 +65,21 @@ declare namespace ext.tabs {
    * Get tab by ID.
    * @param tabId Tab ID.
    */
-  export function get(tabId: string): Promise<ExtTab>
+  export function get(tabId: string): Promise<Tab>
   
   /**
    * Get a list of all tabs matching the filter.
    * @param filter Optional tab filter.
    * @returns Array of tabs matching the filter.
    */
-  export function query(filter?: Partial<ExtTab>): Promise<ExtTab[]>
+  export function query(filter?: Partial<Tab>): Promise<Tab[]>
   
   /**
    * Create a new tab.
    * @param properties Tab creation properties.
    * @returns Created tab.
    */
-  export function create(properties?: ExtTabProperties): Promise<ExtTab>
+  export function create(properties?: TabProperties): Promise<Tab>
 
   /**
    * Destroy one or multiple tabs.
@@ -92,7 +92,7 @@ declare namespace ext.tabs {
    * @param tabIds Tab ids to update.
    * @param properties Tab properties to update.
    */
-  export function update(tabIds: string | string[], properties?: ExtTabProperties): Promise<void>
+  export function update(tabIds: string | string[], properties?: TabProperties): Promise<void>
   
   // Dispatch events
 
@@ -117,21 +117,21 @@ declare namespace ext.tabs {
   // Events
 
   /** Called when a tab is created. */
-  export const onCreated: ExtTabHandler<(event: ExtTabEvent, tab: ExtTab) => void>
+  export const onCreated: EventHandler<(event: TabEvent, tab: Tab) => void>
 
   /** Called when a tab is removed. */
-  export const onRemoved: ExtTabHandler<(event: ExtTabEvent, tab: ExtTab) => void>
+  export const onRemoved: EventHandler<(event: TabEvent, tab: Tab) => void>
 
   /** Called when tab properties are updated. */
-  export const onUpdated: ExtTabHandler<(event: ExtTabEvent, tab: ExtTab) => void>
+  export const onUpdated: EventHandler<(event: TabEvent, tab: Tab) => void>
 
   /** Called when tab is clicked or the click event is manually dispatched. */
-  export const onClicked: ExtTabHandler<(event: ExtTabEvent, tab: ExtTab) => void>
+  export const onClicked: EventHandler<(event: TabEvent, tab: Tab) => void>
 
   /** Called when tab sound icon is clicked or the event is manually dispatched. */
-  export const onClickedMute: ExtTabHandler<(event: ExtTabEvent, tab: ExtTab) => void>
+  export const onClickedMute: EventHandler<(event: TabEvent, tab: Tab) => void>
 
   /** Called when tab close icon is clicked or the event is manually dispatched. */
-  export const onClickedClose: ExtTabHandler<(event: ExtTabEvent, tab: ExtTab) => void>
+  export const onClickedClose: EventHandler<(event: TabEvent, tab: Tab) => void>
 
 }
